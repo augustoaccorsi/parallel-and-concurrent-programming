9c1c5c39cf83bf87cb5444b1279d599f3c5cd4e3
augustoaccorsi <augusto.acorsi@gmail.com>
2021-11-02 22:57:11 -0300

    add deadlocks

---
+++ parallel-and-concurrent-programming/src/com/augustoaccorsi/chapter5/abandonedlock/AbandonedLockDemo.java
@@ -0,0 +1,52 @@
+package com.augustoaccorsi.chapter5.abandonedlock;
+
+import java.util.concurrent.locks.*;
+
+class Philosopher extends Thread {
+
+    private Lock firstChopstick, secondChopstick;
+    private static int sushiCount = 500;
+
+    public Philosopher(String name, Lock firstChopstick, Lock secondChopstick) {
+        this.setName(name);
+        this.firstChopstick = firstChopstick;
+        this.secondChopstick = secondChopstick;
+    }
+
+    public void run() {
+        while(sushiCount > 0) { // eat sushi until it's all gone
+
+            // pick up chopsticks
+            firstChopstick.lock();
+            secondChopstick.lock();
+
+            try {
+                // take a piece of sushi
+                if (sushiCount > 0) {
+                    sushiCount--;
+                    System.out.println(this.getName() + " took a piece! Sushi remaining: " + sushiCount);
+                }
+
+                if (sushiCount == 10)
+                    System.out.println(1 / 0);
+            }
+            finally {
+                // put down chopsticks
+                secondChopstick.unlock();
+                firstChopstick.unlock();
+            }
+
+        }
+    }
+}
+
+public class AbandonedLockDemo {
+    public static void main(String[] args) {
+        Lock chopstickA = new ReentrantLock();
+        Lock chopstickB = new ReentrantLock();
+        Lock chopstickC = new ReentrantLock();
+        new Philosopher("Barron", chopstickA, chopstickB).start();
+        new Philosopher("Olivia", chopstickB, chopstickC).start();
+        new Philosopher("Steve", chopstickA, chopstickC).start();
+    }
+}
